a1b5e68fcd4a1edaf41870f7001c5a0877cc94db
JeliPenguin <jeffbenchu@gmail.com>
2023-02-03 15:12:07 +0000

    transition cost update

--- Code/p1_load_save.py
+++ Code/p1_load_save.py
@@ -3,7 +3,7 @@ from joblib import load
 
 def showStats(alg):
     print("-"*100)
-    comparedAlg = [alg, "bfs","dfs",alg+"T", "bfsT","dfsT"]
+    comparedAlg = [alg, "bfs", "dfs", alg+"T", "bfsT", "dfsT"]
     metrics = ["total_cell_visted", "total_travel_cost"]
     summary(comparedAlg, metrics)
     # for alg in algorithmNames:
@@ -15,6 +15,12 @@ def showStats(alg):
     print("-"*100)
 
 
+def compareAD():
+    metrics = ["total_cell_visted", "total_travel_cost"]
+    for m in metrics:
+        compare("dijkT", "astarT", m)
+
+
 def summary(algs, metrics):
     for m in metrics:
         print("\nEvaluating "+m)
@@ -24,27 +30,29 @@ def summary(algs, metrics):
             print(f"{a}: {sum(costs)}")
 
 
-# def compare(alg1, alg2, metric):
-#     print("\nEvaluating: "+metric)
-#     path1 = f"../save/{alg1}/"
-#     cost1 = load(path1+metric)
-#     path2 = f"../save/{alg2}/"
-#     cost2 = load(path2+metric)
-#     print(metric+" sums:")
-#     print(f"{alg1} sum: ", sum(cost1))
-#     print(f"{alg2} sum: ", sum(cost2))
-#     print("\n")
-#     # for i in range(len(cost1)):
-#         t1 = cost1[i]
-#         t2 = cost2[i]
-#         if t1 > t2:
-#             maxAlg = alg1
-#         elif t2 > t1:
-#             maxAlg = alg2
+def compare(alg1, alg2, metric):
+    print("\nEvaluating: "+metric)
+    path1 = f"../save/{alg1}/"
+    cost1 = load(path1+metric)
+    path2 = f"../save/{alg2}/"
+    cost2 = load(path2+metric)
+    print(metric+" sums:")
+    print(f"{alg1} sum: ", round(sum(cost1), 2))
+    print(f"{alg2} sum: ", round(sum(cost2), 2))
+    print("\n")
+    for i in range(len(cost1)):
+        t1 = round(cost1[i], 2)
+        t2 = round(cost2[i], 2)
+        if t1 != t2:
+            if t1 > t2:
+                maxAlg = alg1
+            elif t2 > t1:
+                maxAlg = alg2
 
-#         print(f"{maxAlg} has greater {metric} of: {max(t1,t2)} for bin {i+1}")
+            print(f"{maxAlg} has greater {metric} of: {max(t1,t2)} for bin {i+1}")
 
 
 # algorithmNames = ["dijk", "dijkT", "astar"]
-algorithmName = "astar"
-showStats(algorithmName)
+# algorithmName = "astar"
+# showStats(algorithmName)
+compareAD()
